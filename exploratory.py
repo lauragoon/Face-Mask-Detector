4896d46c0660a26ba5bedf801e4fadafe14c021a
Claire Wang <cw74@rice.edu>
2020-10-19 23:21:30 -0500

    update

--- exploratory.py
+++ exploratory.py
@@ -7,4 +7,11 @@ mask = tf.io.read_file('./Face-Mask-Detector/0mask.jpg')
 decodedmask = tf.image.decode_image(mask,3)
 decodednomask = tf.image.decode_image(nomask,3)
 
+
+#get tensor output
 print(decodedmask)
+print(decodednomask)
+
+#get average rgb
+print(tf.reduce_mean(tf.reduce_mean(decodedmask, 1), 0))
+print(tf.reduce_mean(tf.reduce_mean(decodednomask, 1), 0))
