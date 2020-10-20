4281a98ec366a522afe273916c7508f950a16e06
Laura Goon <goon.laura@gmail.com>
2020-10-20 18:50:33 -0400

    update structure

--- exploratory.py
+++ exploratory.py
@@ -1,7 +1,7 @@
 import tensorflow as tf 
 
-nomask = tf.io.read_file('./Face-Mask-Detector/0nomask.png')
-mask = tf.io.read_file('./Face-Mask-Detector/0mask.jpg')
+nomask = tf.io.read_file('media/Face-Mask-Detector/0nomask.png')
+mask = tf.io.read_file('media/Face-Mask-Detector/0mask.jpg')
 
 
 decodedmask = tf.image.decode_image(mask,3)
