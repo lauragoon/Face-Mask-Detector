1a15dae1fc82168da19acb5bf866e08c38e28d41
Laura Goon <goon.laura@gmail.com>
2020-11-19 00:11:00 -0500

    test upload submit file response

--- scripts.js
+++ scripts.js
@@ -1,15 +1,54 @@
-function readUrl(input) {
+function readUrlKNN(input) {
   
   if (input.files && input.files[0]) {
     let reader = new FileReader();
     reader.onload = (e) => {
       let imgData = e.target.result;
       let imgName = input.files[0].name;
-      input.setAttribute("data-title", imgName + 'hi');
+      input.setAttribute("data-title", imgName);
       console.log(e.target.result);
       console.log(imgName);
     }
     reader.readAsDataURL(input.files[0]);
+
+    $("knn-p").append("HELLO")
+
+  }
+
+}
+
+function readUrlSVM(input) {
+  
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let imgData = e.target.result;
+      let imgName = input.files[0].name;
+      input.setAttribute("data-title", imgName);
+      console.log(e.target.result);
+      console.log(imgName);
+    }
+    reader.readAsDataURL(input.files[0]);
+
+    
+  }
+
+}
+
+function readUrlCNN(input) {
+  
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let imgData = e.target.result;
+      let imgName = input.files[0].name;
+      input.setAttribute("data-title", imgName);
+      console.log(e.target.result);
+      console.log(imgName);
+    }
+    reader.readAsDataURL(input.files[0]);
+
+    
   }
 
 }
