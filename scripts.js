654e123c6d82c73d6120656f05b26311b453081e
Laura Goon <goon.laura@gmail.com>
2020-11-18 23:58:17 -0500

    test ui

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function readUrl(input) {
     reader.onload = (e) => {
       let imgData = e.target.result;
       let imgName = input.files[0].name;
-      input.setAttribute("data-title", imgName);
+      input.setAttribute("data-title", imgName + 'hi');
       console.log(e.target.result);
       console.log(imgName);
     }
