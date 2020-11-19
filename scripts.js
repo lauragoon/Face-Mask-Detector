16bfb8c4ecf3d8835c8f500b1b510224f6bf83a6
Laura Goon <goon.laura@gmail.com>
2020-11-19 01:31:40 -0500

    test upload 1006

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
-var fileUpload;
+var fileUploadKNN;
+var fileUploadSVM;
+var fileUploadCNN;
 
 window.onload = function () {
-  var fileUpload = document.getElementById('fileUpload');
-  fileUpload.onchange = readImage;
+  var fileUploadKNN = document.getElementById('fileUpload-knn');
+  fileUploadKNN.onchange = readImageKNN;
+  var fileUploadKNN = document.getElementById('fileUpload-svm');
+  fileUploadSVM.onchange = readImageSVM;
+  var fileUploadKNN = document.getElementById('fileUpload-cnn');
+  fileUploadCNN.onchange = readImageCNN;
 };
 
-function readImage() {
+function readImageKNN() {
   console.log("HERE 1");
 
   if ( this.files && this.files[0] ) {
@@ -20,7 +26,51 @@ function readImage() {
 
     console.log("HERE 3");
     
-    var div = document.getElementById('tester');
+    var div = document.getElementById('knn-ret');
+    div.innerHTML += 'HI';
+
+    console.log("DID THIS")
+  }
+}
+
+function readImageSVM() {
+  console.log("HERE 1");
+
+  if ( this.files && this.files[0] ) {
+    console.log("HERE 2");
+
+    var FR= new FileReader();
+    FR.onload = function(e) {
+     var img = new Image();
+     img.src = e.target.result;
+    };       
+    FR.readAsDataURL( this.files[0] );
+
+    console.log("HERE 3");
+    
+    var div = document.getElementById('svm-ret');
+    div.innerHTML += 'HI';
+
+    console.log("DID THIS")
+  }
+}
+
+function readImageCNN() {
+  console.log("HERE 1");
+
+  if ( this.files && this.files[0] ) {
+    console.log("HERE 2");
+
+    var FR= new FileReader();
+    FR.onload = function(e) {
+     var img = new Image();
+     img.src = e.target.result;
+    };       
+    FR.readAsDataURL( this.files[0] );
+
+    console.log("HERE 3");
+    
+    var div = document.getElementById('cnn-ret');
     div.innerHTML += 'HI';
 
     console.log("DID THIS")
